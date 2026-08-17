a33a2d0bbdab2d4151340bcd2089353c20712bf9
The-estray <317675240+The-estray@users.noreply.github.com>
2026-08-17 21:32:33 +0300

    feat: implement base Character class

---
+++ classes/character.php
@@ -0,0 +1,64 @@
+<?php
+
+class Character{
+    public string $name;
+    protected int $health;
+    protected int $maxHealth;
+    protected int $attackPower;
+    protected int $defense;
+
+    public function __construct(string $name, int $health, int $attackPower, int $defense)
+    {
+        $this->name = $name;
+        $this->health = $health;
+        $this->attackPower = $attackPower;
+        $this->defense = $defense;
+        $this->maxHealth = $health;
+    }
+
+    public function takeDamage(int $rawDamage)
+    {
+        if ($this->defense >= $rawDamage) {
+            $this->defense -= $rawDamage;
+            return 0;
+        }
+
+        elseif ($this->defense < $rawDamage) {
+            $rawDamage -= $this->defense;
+            if ($this->health >= $rawDamage) {
+                $this->health -= $rawDamage;
+                return $rawDamage;
+            } else {
+                $this->health = 0;
+                return $rawDamage;
+            }
+        }
+    }
+
+    public function isAlive() : bool
+    {
+        if ($this->health > 0) return true;
+        return false;
+    }
+
+    public function attack(object $target): string
+    {
+        $damage = $target->takeDamage($this->attackPower);
+        return "{$this->name} attacked {$target->getname()}, {$this->name} dealt {$damage} damage to {$target->getname()}";
+    }
+
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    public function getHealth()
+    {
+        return $this->health;
+    }
+
+    public function getMaxHealth()
+    {
+        return $this->maxHealth;
+    }
+}
